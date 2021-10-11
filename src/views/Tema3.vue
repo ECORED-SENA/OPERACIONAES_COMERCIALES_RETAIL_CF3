3ff9992d34913b408d6235c5e004618778828146
luisEfe80 <luismendez80@hotmail.com>
2021-10-11 10:48:09 -0400

    Ajuste de espaciados

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -3,48 +3,48 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
-  .titulo-principal(data-aos="flip-up")
-    .titulo-principal__numero
-      span 3
-    h1 Muebles en el punto de venta
+    .titulo-principal(data-aos="flip-up")
+      .titulo-principal__numero
+        span 3
+      h1 Muebles en el punto de venta
 
-  #t_3_1.titulo-segundo
-    h2 3.1  Tipos de muebles
-
-
-
-  Separador
-  br
-  br
-  #t_3_2.titulo-segundo
-    h2 3.2  Rotación de las categorías
+    #t_3_1.titulo-segundo
+      h2 3.1  Tipos de muebles
 
 
 
-
-  Separador
-  br
-  br
-  #t_3_2.titulo-segundo
-    h2 3.3  Codificación de productos
+    Separador
+    br
+    br
+    #t_3_2.titulo-segundo
+      h2 3.2  Rotación de las categorías
 
 
 
 
-  Separador
-  br
-  br
-  #t_3_4.titulo-segundo
-    h2 3.4  El precio
+    Separador
+    br
+    br
+    #t_3_2.titulo-segundo
+      h2 3.3  Codificación de productos
 
 
 
 
-  Separador
-  br
-  br
-  #t_3_4.titulo-segundo
-    h2 Activación de marca
+    Separador
+    br
+    br
+    #t_3_4.titulo-segundo
+      h2 3.4  El precio
+
+
+
+
+    Separador
+    br
+    br
+    #t_3_5.titulo-segundo
+      h2 3.5 Activación de marca
 
 
 
