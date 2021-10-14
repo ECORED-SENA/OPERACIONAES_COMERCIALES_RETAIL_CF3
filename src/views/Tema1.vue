79e1eb3ae46b3682906f3a25f6ade11e11b6ddf2
luisEfe80 <luismendez80@hotmail.com>
2021-10-14 11:37:11 -0400

    Adicion de contenido

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -7,10 +7,71 @@
       .titulo-principal__numero
         span 1
       h1 <i>Retail</i>
+    .row.mb-5
+      .col-lg-7.slideInUp
+        p.mb-3  <b>La venta al detalle o <i>retail</i>, es un sistema de comercialización que entrega el producto al cliente y en este sentido, los canales de venta son vitales para el mismo y para lograr la diferenciación comercial, las empresas dedican gran parte de su esfuerzo en el diseño y experiencia de compra.</b> Además, la industria del <i>retail</i> se percibe como un negocio habitual, porque la sociedad piensa que se trata de empresas enfocadas en alcanzar buenas negociaciones, lo que se logra haciendo presión a los proveedores para poder vender los bienes y servicios a precios bajos.
+        p.mb-3  Sin embargo, el mundo del <i>retail</i> ha ido evolucionando bastante en los últimos años. Empresas de grandes superficies o grandes almacenes como Carrefour, El Corte Inglés y Walmart, cada vez se vuelven más sofisticadas.
+        p.mb-3  Un <i>retail</i> debe lograr la conexión emocional con el cliente y para poderlo lograr el bien o servicio ofrecido debe estar alineado con los valores, deseos, necesidades, poder económico y expectativas de los clientes y, además, se debe diferenciar de la competencia. Diferencia que debe ir unida a la capacidad de ofrecer una experiencia de compra positiva y memorable, vital para crear <b>Brand Equity</b> (aumentar el tráfico, vender y fidelizar al cliente).
+      .col-lg-5
+        figure.slideInLeft
+          img(src='@/assets/curso/t1-i1.png', alt='imagen decorativa')
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        .cajon.color-primario.p-4.mb-4
+          p.mb-0  Una marca debe ser más que un distintivo visual, un bien o un servicio. Su lugar debe estar en la mente o en el corazón de los clientes, de tal forma que esté posicionada racional o emocionalmente. La marca perdura en el tiempo siempre y cuando esté posicionada.
+
+
 
     #t_1_1.titulo-segundo
       h2 1.1  Establecimiento comercial
+    .row.mb-5
+      .col-lg-2
+        figure(data-aos="flip-left")
+          img.nW(src='@/assets/curso/t1-ic1.svg', alt='imagen decorativa')
+      .col-lg-10
+        p.mb-3  Es el lugar donde se despliega una actividad de carácter comercial, no es donde se fabrican los bienes o servicios, es donde se comercializan, por esa razón, se refiere a un punto de venta. Y este punto de venta es el espacio físico en el que se pueden vender y comprar los productos.
+        p Para poder tener un establecimiento comercial se necesita que se tengan los siguientes elementos:
+    TabsB.color-acento-contenido.mb-5
+      .py-4.py-md-5(titulo="Un espacio físico" :icono="require('@/assets/curso/t1-t1.svg')")
+        .row.justify-content-center 
+          .col-md-4
+            figure.slideInLeft
+              img(src='@/assets/curso/t1-i2.jpg', alt='imagen decorativa')
+          .col-md-6.mb-4.mb-md-0
+            .h4 Un espacio físico
+            p Se refiere al local o la tienda donde se ofrecen los productos. Dependiendo del tipo de establecimiento comercial, el espacio físico puede contar con parqueadero, sala de ventas, servicios sanitarios y otros servicios adicionales que sirven para mejorar la atención al cliente.
 
+      .py-4.py-md-5(titulo="Productos" :icono="require('@/assets/curso/t1-t2.svg')")
+        .row.justify-content-center 
+          .col-md-4
+            figure.slideInLeft
+              img(src='@/assets/curso/t1-i2-1.jpg', alt='imagen decorativa')
+          .col-md-6.mb-4.mb-md-0
+            .h4 Productos
+            p Son todos los bienes y servicios que se ofrecen en el espacio físico. Si el establecimiento es una relojería, los productos son relojes; si es una empresa de telecomunicaciones, un servicio podría ser la conexión a internet.
+
+      .py-4.py-md-5(titulo="Vendedores" :icono="require('@/assets/curso/t1-t3.svg')")
+        .row.justify-content-center 
+          .col-md-4
+            figure.slideInLeft
+              img(src='@/assets/curso/t1-i2-2.jpg', alt='imagen decorativa')
+          .col-md-6.mb-4.mb-md-0
+            .h4 Vendedores
+            p Son la fuerza de ventas del establecimiento y los que se van a encargar de atender, asesorar y vender los productos a los clientes. En otras palabras, son quienes logran que los clientes efectúen las acciones de compra.
+
+      .py-4.py-md-5(titulo="Compradores" :icono="require('@/assets/curso/t1-t4.svg')")
+        .row.justify-content-center 
+          .col-md-4
+            figure.slideInLeft
+              img(src='@/assets/curso/t1-i2-3.jpg', alt='imagen decorativa')
+          .col-md-6.mb-4.mb-md-0
+            .h4 Compradores
+            p Estos son las personas, clientes y consumidores que llegan al establecimiento para hacer una acción de compra, bien sea de un bien o servicio. 
+
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        .cajon.color-primario.p-4.mb-4
+          p.mb-0  Con estos cuatro componentes se puede generar la transacción de compra y venta en cualquier establecimiento comercial.
 
 
 
@@ -19,6 +80,31 @@
     br
     #t_1_2.titulo-segundo
       h2 1.2  Formatos comerciales
+
+    .row.mb-5
+      .col-lg-7(data-aos="flip-up")
+        p.mb-3  En Colombia existe una tendencia que ha gustado en los clientes; <b>se trata de preferir nuevos formatos y canales comerciales de venta directa e independiente con precios económicos en distintos productos.</b> Así, se obtiene un gran beneficio a través de productos que se consideran de buena calidad, presentación, que están distribuidos en espacios limpios y atendidos por un personal capacitado y joven. 
+        p.mb-3  D1, Ara y Justo & Bueno son ejemplo de lo anterior, presentan un comportamiento dinámico y novedoso teniendo una excelente aceptación por parte de la sociedad. Pertenecen al sector del descuento duro (<i>hard discount</i>) y se caracterizan por ofrecer precios económicos, especialmente de la canasta familiar, frente a los establecimientos tradicionales, lo que ha logrado que muchos clientes los prefieran a la hora de hacer sus compras. <b>Sus tiendas son sencillas, no hacen inversión publicitaria, poseen pocos empleados, no cuentan con impulsadores y ofrecen productos de pequeños y medianos proveedores que, con precios económicos, intentan permanecer en el mercado.</b>
+      .col-lg-5
+        figure(data-aos="flip-left")
+          img(src='@/assets/curso/t1-i3.jpg', alt='imagen decorativa')
+
+    .row.mb-5.justify-content-center
+      .col-lg-10
+        .cajon.color-primario.p-4.mb-4
+          p.mb-0  Con estos cambios y evolución en el sector retail se ha generado una transformación en el comportamiento del consumidor y sus hábitos de compra. Los productos de marca propia que se ofrecen en estas tiendas de venta al detal se presentan como una nueva alternativa para el cliente, y que, además, permite su producción a empresas, principalmente pymes colombianas, que hacen su papel como proveedores y gestores de empleo (Vigaray, 2005).
+
+    p.mb-5  A continuación, se presentan los tipos de formatos comerciales tradicionales:
+    .neg-margin-x.bg-curved
+      AcordionA.mb-5(tipo="b" clase-tarjeta="tarjeta tarjeta--azul")
+        .row.justify-content-center(titulo="Titulo item 1")
+          .col-md-10.mb-4.mb-md-0
+            p This is another paragraph. I think it needs to be added that the set of elements tested is not exhaustive in any sense. I have selected those elements for which it can make sense to write user style sheet rules, in my opionion.
+        
+          .col-md-2
+            figure
+              img(src='@/assets/template/img-placeholder.svg', alt='Texto que describa la imagen')
+
 
 
 
